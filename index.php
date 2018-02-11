c2473c359518ebd9417449117043c79461f69576
Gento <a551.return@gmail.com>
2018-02-11 21:23:57 +0800

    タグ綺麗になってる

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <?php
   session_start();
 
@@ -28,7 +27,7 @@
   //       unset($_POST["join"]);
 
   // ログインページへ遷移
-       header('Location: post.html');
+       header('Location: json_map.html');
         exit();
 
 
@@ -136,210 +135,177 @@ if (isset($_POST) && !empty($_POST)){
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Where is *(アスタリスク)</title>
     <meta name="Nova theme" content="width=device-width, initial-scale=1">
-    
 
-<!--
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
--->
-          
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css" />
+    <link rel="stylesheet" href="css/responsive.css" />
+    <link rel="stylesheet" href="css/login.css" />
 
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-          rel="stylesheet">
-    <link rel="stylesheet" href="css/style.css"/>
-    <link rel="stylesheet" href="css/responsive.css"/>
-     <link rel="stylesheet" href="css/login.css"/>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
 
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"/>
-    
-     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-<!-- Ryo added -->
-<!-- <script type="text/javascript" src="assets/js/jquery-1.7.2.min.js"></script> -->
-<!-- <script type="text/javascript" src="assets/js/jquery.ba-hashchange.min.js"></script>
-<script type="text/javascript" src="assets/js/autoConfirm.js"></script> -->
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
 
 
 <body>
-
-<!-- Navigation
+    <!-- Navigation
     ================================================== -->
-
 <div class="hero-background">
-   <div>
-      <img class="strips" src="earth.png">
-   </div>
-      <div class="container">
+
+    <img class="strips" src="earth.png">
+
+    <div class="container">
         <div class="header-container header">
-
-
-                      <div class="header-right">
+            <div class="header-right">
                 <a class="navbar-item" href="contact.html">Contact</a>
             </div>
-
-            
         </div>
-        <!--navigation-->
-
-        <!-- Hero-Section
-          ================================================== -->
-      <form method="POST" action="" >
+        <!--end of navigation-->
 
         <div class="hero row">
             <div class="hero-right col-sm-6 col-sm-6">
                 <h1 class="header-headline bold"> 世界の景色をお手軽に <br></h1>
-                <h4 class="header-running-text light"> You can see so easy
-                the view of the world. </h4>
-               
-
-            <button class="hero-btn" > Enter</button>
-                
+                <h4 class="header-running-text light"> You can see so easy the view of the world. </h4>
             </div>
-<!--                    <button class="hero-btn" id="popup" onclick="div_show()"> Enter</button>-->
-<!--                     <button class="hero-btn" > Enter</button> -->
-                
-<!--             </div><!--hero-left--> 
 
-            <div class="col-sm-6 col-sm-6 ">
-                  <div class="loginpanel">
-                  <div class="txt">
-                  <input id="user6" type="text" placeholder="E-mail" name=login_email />
-                  <label for="user" class="entypo-mail"></label>
-                  </div>
-                      <div class="txt">
-                        <input id="pwd7" type="password" placeholder="Password" name=login_password />
-                          <label for="pwd" class="entypo-lock"></label>
-                      </div>
-  
-                  <div class="buttons">
-                    <input type="submit" value="Login" />
-                    <span>
+            <form method="POST" action="">
+                <div class="col-sm-6 col-sm-6 ">
+                    <div class="loginpanel">
+                        <div class="txt">
+                            <input id="user6" type="text" placeholder="E-mail" name="login_email" />
+                            <label for="user" class="entypo-mail"></label>
+                        </div>
+                        <div class="txt">
+                            <input id="pwd7" type="password" placeholder="Password" name="login_password" />
+                            <label for="pwd" class="entypo-lock"></label>
+                        </div>
+
+                    <div class="buttons">
+                            <input type="submit" value="Login" />
+                            <span>
                     <a href="javascript:void(0)" class="entypo-user-add" > Register</a>
                     </span>
-                  </div>
-  
-                  <a href="json_map.html" class="submit_button">
+                        </div>
+
+                        <a href="json_map.html" class="submit_button">
                   <input type="button" value="Visitor" class="submit_button">
                   </a>
 
-<div id="forget_pw">
-  <p>passwordを忘れた方は<a href="#">こちら</a></p>
-  </div>
-  
-  <div class="hr">
-    <div></div>
-    <div>OR</div>
-    <div></div>
-  </div>
-  
-  <div class="social">
-    <a href="javascript:void(0)" class="facebook"></a>
-    <a href="javascript:void(0)" class="twitter"></a>
-    <a href="javascript:void(0)" class="googleplus"></a>
-  </div>
-</div>
-           
-            </div>
+                        <div id="forget_pw">
+                            <p>passwordを忘れた方は<a href="#">こちら</a></p>
+                        </div>
 
-           
-        
-<div class="col-sm-6 col-sm-6">
-
-    <div class="kaiintouroku">
-        <font size="3" color="black">
-            <u>
-            <strong><div align="center"><p>新規会員登録</p></div></strong>
-            </u></font>
-        <div class="txt">
-            <input id="user" type="text" placeholder="NickName" />
-            <label for="user" class="entypo-user-add"></label>
-          <?php if((isset($error["login"])) && ($error["login"]== 'failed')){ ?>
-              <p class="error">* emailかパスワードが間違っています。</p>
-          <?php } ?>
-        </div>
-
-        <div class="txt">
-            <input id="user1" type="text" placeholder="E-mail" />
-            <label for="user" class="entypo-mail"></label>
-        </div>
+                        <div class="hr">
+                            <div></div>
+                            <div>OR</div>
+                            <div></div>
+                        </div>
+                        <div class="social">
+                                <a href="javascript:void(0)" class="googleplus"></a>
+                           
+                        </div>
+                    </div>
+                </div>
 
 
-        <div class="txt">
-            <input id="user2" type="text" placeholder="Check E-mail Address" />
-            <label for="user" class="entypo-mail"></label>
-        </div>
+                <!--新規会員登録-->
+                <div class="col-sm-6 col-sm-6">
+                    <div class="kaiintouroku">
+                        <font size="3" color="black">
+                            <u><strong>
+                            <div align="center"><p>新規会員登録</p></div>
+                            </strong></u>
+                        </font>
+                        <div class="txt">
+                            <input id="user" type="text" placeholder="NickName" />
+                            <label for="user" class="entypo-user-add"></label>
+                            <?php if((isset($error["login"])) && ($error["login"]== 'failed')){ ?>
+                            <p class="error">* emailかパスワードが間違っています。</p>
+                            <?php } ?>
+                        </div>
 
+                        <div class="txt">
+                            <input id="user1" type="text" placeholder="E-mail" />
+                            <label for="user" class="entypo-mail"></label>
+                        </div>
 
-        <div class="txt">
-            <input id="pwd1" type="password" placeholder="Password" />
-            <label for="pwd" class="entypo-lock"></label>
-        </div>
+                        <div class="txt">
+                            <input id="user2" type="text" placeholder="Check E-mail Address" />
+                            <label for="user" class="entypo-mail"></label>
+                        </div>
 
-        <div class="txt">
-            <input id="pwd2" type="password" placeholder="Check Password" />
-            <label for="pwd" class="entypo-lock"></label>
-        </div>
+                        <div class="txt">
+                            <input id="pwd1" type="password" placeholder="Password" />
+                            <label for="pwd" class="entypo-lock"></label>
+                        </div>
 
-        <div class="txt">
-        </div>
+                        <div class="txt">
+                            <input id="pwd2" type="password" placeholder="Check Password" />
+                            <label for="pwd" class="entypo-lock"></label>
+                        </div>
 
-        <div class="buttons">
-            <!--      <a href="join/index.php"> -->
-            <input type="button" class="hero-btn2" value="Confirm Account" >
+                        <div class="txt">
+                        </div>
 
-        </div>
+                        
 
-        <!--hero-->
+                        <div class="buttons">
+                            <!--      <a href="join/index.php"> -->
+                            <input type="button" class="hero-btn2" value="Confirm Account">
+                        </div>
+                    </div>
+                </div>
+                <!--end of 新規会員登録-->
 
-    </div>
-    <!--hero-container-->
-</div>
+               <!--会員登録確認-->
+                <div class="col-sm-6 col-sm-6">
+                    <div class="kaiintouroku2">
+                        <font size="3" color="black">
+                            <u><strong>
+                            <div align="center">
+                            <p>会員登録確認</p>
+                            </div>
+                            </strong></u>
+                        </font>
+                        
+                        <div class="txt">
+                            <input id="nick_name" type="text" placeholder="NickName" name="nick_name" readonly />
+                            <label for="user" class="entypo-user-add"></label>
+                        </div>
 
+                        <div class="txt">
+                            <input id="email" type="text" placeholder="E-mail" name="email" readonly/>
+                            <label for="user" class="entypo-mail"></label>
+                        </div>
 
-<div class="col-sm-6 col-sm-6">
+                        <div class="txt">
+                            <input id="password" type="password" placeholder="Password" name="password" readonly/>
+                            <label for="pwd" class="entypo-lock"></label>
+                        </div>
+                        <!--         <div class="buttons"> -->
+                        <input type="submit" class="hero-btn2" value="Create Account" />
+                    </div>
+                </div>
+                <!--end of 会員登録確認-->
+                
+            </form>
+        </div><!--end of hero row-->
+    </div><!--end of container-->
+</div><!--end of hero-background-->
+    
+    
 
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
 
-    <div class="kaiintouroku2">
-        <font size="3" color="black">
-        <u>
-        <strong><div align="center"><p>会員登録確認</p></div></strong>
-        </u></font>
-        <div class="txt">
-            <input id="nick_name" type="text" placeholder="NickName" name="nick_name" readonly />
-            <label for="user" class="entypo-user-add"></label>
-        </div>
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 
-        <div class="txt">
-            <input id="email" type="text" placeholder="E-mail" name="email" readonly/>
-            <label for="user" class="entypo-mail"></label>
-        </div>
-
-        <div class="txt">
-            <input id="password" type="password" placeholder="Password" name="password" readonly/>
-            <label for="pwd" class="entypo-lock"></label>
-        </div>
-<!--         <div class="buttons"> -->
-            <input type="submit" class="hero-btn2" value="Create Account"  />
-
-    </div>
-
-
-
-    </div>           
-            <!--hero-background-->
-      </form>
-
-<script src="https://code.jquery.com/jquery-3.2.1.min.js"
-        integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-        crossorigin="anonymous"></script>
-        
-        <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-
-<script src="js/login.js"></script>
-<script src="js/script.js"></script>
+    
+    <script src="js/login.js"></script>
+    <script src="js/script.js"></script>
 
 </body>
 
