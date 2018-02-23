acb7f8e8112eb5d8f7bb45a7908218389ac28bfc
佐藤　華陽 <p100slp33@gmail.com>
2018-02-23 16:59:31 +0800

    html -> php

--- help.php
+++ help.php
@@ -74,13 +74,15 @@
     </div> -->
 
 <header>
-   <a class="navbar-brand logo" href="#"></a>
+   <a class="navbar-brand logo" href="index.php"></a>
     <div class=" topnav" id="myTopnav"> 
-      <a href="index.html">Logout</a>
-      <a class="active" href="contact.html">Contact</a>
-      <a href="profile.html">MyPage</a>
-      <a href="post.html">POST</a>
-      <a href="json_map.html">*MAP*</a>
+      <?php if (isset($_SESSION["id"])){ ?>
+      <a href="logout.php">Logout</a>
+      <a href="profile.php">MyPage</a>
+      <a href="post.php">POST</a>
+      <?php } ?>
+      <a class="active" href="contact.php">Contact</a>
+      <a href="json_map.php">*MAP*</a>
       <a href="javascript:void(0);" style="font-size:30px;" class="icon" onclick="myFunction()">&#9776;</a>
     </div>
 </header>
